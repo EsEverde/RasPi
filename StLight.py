71b96126e6e4d9ecacf3016e92b10b96e5c0b320
Everardo Gonzalez <everardo.gonzalez@cos.uni-heidelberg.de>
2016-02-14 13:24:17 +0100

    Added Startup light

---
+++ StLight.py
@@ -0,0 +1,14 @@
+import sys
+import opc
+numLEDs =30 #Init LEDs
+MaxLED=64
+Strips=5
+client = opc.Client('localhost:7890')
+pixels=[ (255,250,240) ] * (64 * Strips)
+
+client.put_pixels(pixels)
+
+            
+                    
+
+
