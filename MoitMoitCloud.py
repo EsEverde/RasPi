46ab2aadac0b7609a999a12a34ffab8ca2e98ce7
Everardo Gonzalez <everardo.gonzalez@cos.uni-heidelberg.de>
2016-04-21 22:24:14 +0200

    Added new programm MoitMoitCloud.py

--- MoitMoitCloud.py
+++ MoitMoitCloud.py
@@ -13,6 +13,8 @@ client = opc.Client('localhost:7890')
 pixels=[ (255,250,240) ] * (MaxLED * Strips)
 Color1=colorsys.hsv_to_rgb(0.3,1,1)
 Color2=colorsys.hsv_to_rgb(0.44,1,1)
+Color1=[x*255 for x in Color1]
+Color2=[x*255 for x in Color2]
 
 while True: #Main loop
     for i in range(0,Strips):
