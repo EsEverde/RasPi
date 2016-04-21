40319098a2dabf130be66f9e7392ce493051834e
Everardo Gonzalez <everardo.gonzalez@cos.uni-heidelberg.de>
2016-04-21 22:58:28 +0200

    Debugging new programm MoitMoitCloud.py

--- MoitMoitCloud.py
+++ MoitMoitCloud.py
@@ -12,7 +12,7 @@ Strips=5
 client = opc.Client('localhost:7890')
 pixels=[ (255,250,240) ] * (MaxLED * Strips)
 Color1=colorsys.hsv_to_rgb(0.05,1,1)
-Color2=colorsys.hsv_to_rgb(0.09,1,1)
+Color2=colorsys.hsv_to_rgb(0.1,1,1)
 Color1=[x*255 for x in Color1]
 Color2=[x*255 for x in Color2]
 
@@ -20,11 +20,11 @@ while True: #Main loop
     
     for j in range(0,numLEDs):
         for i in range(0,Strips):
-            pixels[i*MaxLED+j+randint(0,3)]= Color1
-            pixels[(i*MaxLED+j+(numLEDs/2)+randint(0,3))%(MaxLED*Strips)]= Color2
+            pixels[i*MaxLED+j]= Color1
+            pixels[(i*MaxLED+j+(numLEDs/2))%(MaxLED*Strips)]= Color2
 
         client.put_pixels(pixels)
-        time.sleep(0.8)
+        time.sleep(0.9)
 
             
                     
