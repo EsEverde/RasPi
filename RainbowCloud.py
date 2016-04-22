fa71a55cb81654fb7aba5f0cc7f69ec3c35a73ea
Everardo Gonzalez <everardo.gonzalez@cos.uni-heidelberg.de>
2016-04-22 22:28:14 +0200

    Lowered satuaration in Rainblowcloud.py

--- RainbowCloud.py
+++ RainbowCloud.py
@@ -28,7 +28,7 @@ while True: #Main loop
         for i in range(0,Strips):
             for j in range(0,numLEDs):
                     
-                Color1=colorsys.hsv_to_rgb((RangeMin+k+(RangeMax-RangeMin)/numLEDs*j+(RangeMax-RangeMin)/Strips*i)%255/255,1,1)
+                Color1=colorsys.hsv_to_rgb((RangeMin+k+(RangeMax-RangeMin)/numLEDs*j+(RangeMax-RangeMin)/Strips*i)%255/255,0.5,1)
                 Color1=[x*255 for x in Color1]
 
                     #Color2=colorsys.hsv_to_rgb(1/numLEDs*(j+15)%numLEDs,1,1/10*k*2)
